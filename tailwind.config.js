fcaf8af29df92a6b143d80c5e86c843dc1b98a7f
tawhidlimon-dev <tawhidlimon.info@gmail.com>
2023-12-25 20:08:46 +0600

    project all done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        headerbg: "#282828",
+        primary: "#F40404"
+      },
+      maxWidth: {
+        container: "1144px",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif;"
+      },
+      backgroundImage: {
+        'banner': "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('images/bannerbg.jpg')",
+      }
+    }  
+  }
+}
